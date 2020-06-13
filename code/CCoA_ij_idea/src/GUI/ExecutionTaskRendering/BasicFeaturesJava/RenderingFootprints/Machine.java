e86864780ff2af6663a98c1c817ed2357280ab87
Georgia FrankinStain <georgiafrankinstain@gmail.com>
2020-06-14 03:09:43 +0700

    add image machine on JavaFX

--- code/CCoA_ij_idea/src/GUI/ExecutionTaskRendering/BasicFeaturesJava/RenderingFootprints/Machine.java
+++ code/CCoA_ij_idea/src/GUI/ExecutionTaskRendering/BasicFeaturesJava/RenderingFootprints/Machine.java
@@ -4,18 +4,32 @@ import GUI.ExecutionTaskRendering.BasicFeaturesJava.RenderingFootprint;
 import GUI.StatementTaskRendering.DataFootprintForRendering;
 import Logic.FootprintSpaceTime.PolygonExtended;
 import Logic.GlobalVariable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+import java.awt.*;
+import java.io.File;
+
 public class Machine extends Pane implements RenderingFootprint {
-    Rectangle rectangle;
+//    Rectangle rectangle;
+    ImageView imageView;
 
     public Machine(DataFootprintForRendering newProperties) {
         setTranslateX(newProperties.getLocalOriginForPointMovingObject().getCoordinats().getX());
         setTranslateY(newProperties.getLocalOriginForPointMovingObject().getCoordinats().getY());
-        this.rectangle = createRectangle(newProperties.getMovingObject().getPolygonExtended());
-        getChildren().addAll(this.rectangle);
+
+//        Image image = new Image("file:src.png",true);
+//        ImageView imageView = new ImageView(image);
+
+
+
+        this.imageView = createRectangle(newProperties.getMovingObject().getPolygonExtended());
+        getChildren().addAll(imageView);
+//        getChildren().addAll(this.rectangle);
     }
 
 
@@ -28,7 +42,7 @@ public class Machine extends Pane implements RenderingFootprint {
 
 
     //==== <start> <Private_Methods> =======================================================================
-    private Rectangle createRectangle(PolygonExtended formMachine) {
+    private ImageView createRectangle(PolygonExtended formMachine) {
         if (formMachine.getCountPoints() != 4) {
             assert(false);
             return null;
@@ -44,7 +58,14 @@ public class Machine extends Pane implements RenderingFootprint {
             return null;
         }
 
-        return new Rectangle(width, height, Color.RED);
+
+        ImageView imageView = new ImageView();
+        File file = new File("D:\\AF\\2019 - 11 - 03 - Centralized_Control_of_Autopilots__diploma\\code\\CCoA_ij_idea\\sourse_files\\machine.png");
+        Image image = new Image(file.toURI().toString());
+        imageView.setImage(image);
+        imageView.setFitWidth(width);
+        imageView.setFitHeight(height);
+        return imageView;
     }
 
     //==== <end> <Private_Methods> =======================================================================
