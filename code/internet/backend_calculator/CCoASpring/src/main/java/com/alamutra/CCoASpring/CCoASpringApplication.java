8ab7d011f5bf1c2f0692120874f14f697691a69d
GeorgiaFrankinStain <georgiafrankinstain@gmail.com>
2022-11-05 01:33:10 +0700

    add generate spring backend_calculator

---
+++ code/internet/backend_calculator/CCoASpring/src/main/java/com/alamutra/CCoASpring/CCoASpringApplication.java
@@ -0,0 +1,13 @@
+package com.alamutra.CCoASpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CCoASpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CCoASpringApplication.class, args);
+	}
+
+}
