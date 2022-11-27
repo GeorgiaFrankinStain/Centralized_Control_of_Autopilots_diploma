eb4acd0fd3c3ef3f77d5bee7f20f9dfde00fc4f6
GeorgiaFrankinStain <georgiafrankinstain@gmail.com>
2022-11-27 18:08:15 +0700

    add moved rectangle in frontend

--- code/internet/frontend/react-ccoa/src/Canvas.tsx
+++ code/internet/frontend/react-ccoa/src/Canvas.tsx
@@ -1,5 +1,39 @@
+import {useEffect, useRef} from "react";
+
 const Canvas = () => {
-    return <canvas />;
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+
+        if (!canvas) {
+            return;
+        }
+
+        canvas.width = 400;
+        canvas.height = 400;
+
+        const context = canvas.getContext('2d');
+
+        if (!context) {
+            return;
+        }
+        let x = 50;
+        context.fillStyle = 'blue';
+        context.fillRect(0, 0, 100, 100);
+        setInterval(function() {
+            context.fillStyle = 'white';
+            context.fillRect(0,0, canvas.width, canvas.height);
+
+            context.fillStyle = 'red';
+            context.fillRect(x = x + 5, 50, 300, 200);
+
+            context.fill();
+
+        }, 25)
+    }, []);
+
+    return <canvas ref={canvasRef} />;
 };
 
 export default Canvas;
