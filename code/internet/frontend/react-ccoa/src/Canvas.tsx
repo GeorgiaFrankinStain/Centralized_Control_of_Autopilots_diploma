8b0284018ddd4798fe92033088a55345364cf3fe
GeorgiaFrankinStain <georgiafrankinstain@gmail.com>
2022-12-15 13:56:10 +0700

    add useAnimationFrame

--- code/internet/frontend/react-ccoa/src/Canvas.tsx
+++ code/internet/frontend/react-ccoa/src/Canvas.tsx
@@ -1,51 +1,242 @@
-import {useEffect, useRef} from "react";
+import React, {MutableRefObject, useEffect, useRef} from "react";
+import assert from "assert";
+
+
+
+
+const imitationDataFromGetElbowFootprint = { //формируется PathMovingUniqueDataForRendering
+    "id_moving_unique_object": "235662",
+    "appearanceType": "non-uniform", //non-uniform is simple colorFill polygon
+    "appearancePolygonForm": [ //(half_option_LINK_nEfBkQ, if appearanceType: non-uniform)
+        {"x": "-13", "y": "-13"},
+        {"x": "-13", "y": "13"},
+        {"x": "13", "y": "13"},
+        {"x": "13", "y": "-13"}
+    ],
+    timeSpaceCoordinates: [
+        {"t": "0", "layer": "0", "x": "10", "y": "0", "angle": "0"},
+        {"t": "1", "layer": "0", "x": "10", "y": "10", "angle": "0"},
+        {"t": "2", "layer": "0", "x": "10", "y": "20", "angle": "0"}
+    ]
+}
+
+function getApproximationPosition(
+    position1: number,
+    time1: number,
+    position2: number,
+    time2: number,
+    timeApproximation: number
+): number {
+    const isReverseOrder: boolean = time1 > time2;
+    if (isReverseOrder) {
+        return getApproximationPosition(position2, time2, position1, time1, timeApproximation);
+    } else {
+        return getApproximationAscendingOrderOfTime(position1, time1, position2, time2, timeApproximation);
+    }
+}
+
+function getApproximationAscendingOrderOfTime(
+    position1: number,
+    time1: number,
+    position2: number,
+    time2: number,
+    timeApproximation: number
+):number {
+    // assert(time1 <= timeApproximation);
+    // assert(timeApproximation <= time2);
+
+
+    const percentage = percentageProximity(time1, time2, timeApproximation);
+    const positionInterval = position2 - position1;
+
+    const approximationNumber = position1 + (positionInterval * percentage);
+
+    return approximationNumber;
+
+}
+
+function comparison(a:number, b:number):boolean {
+    const differentPositive: number = Math.abs(a - b);
+    const accuracy = 0.0001;
+    return differentPositive < accuracy;
+}
+
+function percentageProximity(
+    time1: number,
+    time2: number,
+    timeApproximation: number
+): number {
+    const timeInterval = time2 - time1;
+    const isIntervalIs0 = comparison(timeInterval, 0);
+    if (isIntervalIs0) {
+        const inMiddle = 0.5;
+        return inMiddle;
+    }
+    const percentProximity = (timeApproximation - time1) / timeInterval;
+    return percentProximity;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+interface NextAnimationFrameHandler {
+    (progress: number) : void;
+}
+
+type SetParameters = {
+
+    nextAnimationFrameHandler: NextAnimationFrameHandler;
+    // we still want to have "infinite" animations in some cases
+    duration: number;
+    shouldAnimate: boolean;
+}
+
+//
+// const useAnimationFrame = ({
+//                                nextAnimationFrameHandler: NextAnimationFrameHandler,
+//                                // we still want to have "infinite" animations in some cases
+//                                duration = Number.POSITIVE_INFINITY,
+//                                shouldAnimate = true
+//                            }) => {
+
+
+const useAnimationFrame = ({
+                               nextAnimationFrameHandler,
+                               // we still want to have "infinite" animations in some cases
+                               duration = Number.POSITIVE_INFINITY,
+                               shouldAnimate = true
+                           }: SetParameters) => {
+    const frame = React.useRef(0);
+    // keep track of when animation is started
+    const firstFrameTime = React.useRef(performance.now());
+
+    const animate = (now: number) => {
+        // calculate at what time fraction we are currently of whole time of animation
+        let timeFraction = (now - firstFrameTime.current) / duration;
+        if (timeFraction > 1) {
+            timeFraction = 1;
+        }
+
+        if (timeFraction <= 1) {
+            nextAnimationFrameHandler(timeFraction);
+
+            // request next frame only in cases when we not reached 100% of duration
+            if (timeFraction != 1) frame.current = requestAnimationFrame(animate);
+        }
+    };
+
+    React.useEffect(() => {
+        console.log(shouldAnimate);
+        if (shouldAnimate) {
+            firstFrameTime.current = performance.now();
+            frame.current = requestAnimationFrame(animate);
+        } else {
+            cancelAnimationFrame(frame.current);
+        }
+
+        return () => cancelAnimationFrame(frame.current);
+    }, [shouldAnimate]);
+};
+
+
+
 
 const Canvas = () => {
-    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const brickRef = React.useRef()  as MutableRefObject<HTMLDivElement>;
+    const [shouldAnimate, setShouldAnimate] = React.useState(false);
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
+    const reset = () => {
+        brickRef.current.style.left = String(0);
+    };
 
-        if (!canvas) {
-            return;
+    const nextAnimationFrameHandler = (progress: number) => {
+        console.log(progress);
+        const brick = brickRef.current;
+        if (brick) {
+            const currentLeft = Number(brick.style.left.replace("px", "") || 0);
+
+            if (progress < 1) {
+                brick.style.left = `${1000 * progress}px`;
+            } else {
+                setShouldAnimate(false);
+                brick.style.left = `1000px`;
+            }
         }
+    };
 
-        canvas.width = 400;
-        canvas.height = 400;
+    useAnimationFrame({
+        nextAnimationFrameHandler,
+        shouldAnimate,
+        duration: 1000
+    });
 
-        const context = canvas.getContext('2d');
-
-        if (!context) {
-            return;
-        }
-        let x = 50;
-        context.fillStyle = 'blue';
-        context.fillRect(0, 0, 100, 100);
-        setInterval(function() {
-
-
-            context.fillStyle = 'white';
-            context.fillRect(0,0, canvas.width, canvas.height);
-
-
-            context.beginPath();
-            context.beginPath();
-            context.moveTo(0, 0);
-            context.lineTo(100, 50);
-            context.lineTo(50, 100);
-            context.lineTo(0, 90);
-            context.closePath();
-            context.fill();
-
-            context.fillStyle = 'red';
-            context.fillRect(x = x + 15, 50, 300, 200);
-
-            context.fill();
-
-        }, 105)
-    }, []);
-
-    return <canvas ref={canvasRef} />;
+    return (
+        <>
+            <button onClick={() => reset()}>Reset!</button>
+            <main className="path">
+                <div
+                    className="brick"
+                    ref={brickRef}
+                    onClick={() => setShouldAnimate(true)}
+                >
+                    Click me!
+                </div>
+            </main>
+        </>
+    );
 };
+
+
+
+// ReactDOM.render(<Counter />, document.getElementById('app'))
+
+
+//
+// const Canvas = () => {
+//
+//
+//     return <canvas />;
+// };
+
 
 export default Canvas;
